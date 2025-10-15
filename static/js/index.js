58278aeb1da98833190a161c7cdf43745894672c
aelhakie <aly@opsiclear.com>
2025-10-15 16:22:50 -0400

    Implement dataset image carousel in index.html, enhancing the presentation of the new Objects Mix Dataset alongside existing datasets. Update styles in index.css for carousel functionality and add modals for zoomable images. Modify index.js to initialize carousels with specific options. Update image references in the comparison section for clarity.

--- static/js/index.js
+++ static/js/index.js
@@ -26,9 +26,38 @@ $(document).ready(function() {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
-
     });
 
+    // --- START: MODIFIED CAROUSEL INITIALIZATION ---
+
+    // Options for the thumbnail carousels (many items)
+    var thumbnailCarouselOptions = {
+			slidesToScroll: 1,
+			slidesToShow: 5, // Show more thumbnails at once
+			loop: true,
+			infinite: true,
+			autoplay: false,
+    };
+    // Initialize the two thumbnail carousels with these options
+    bulmaCarousel.attach('#comparison-thumbnail-carousel', thumbnailCarouselOptions);
+    bulmaCarousel.attach('#infill-thumbnail-carousel', thumbnailCarouselOptions);
+
+
+    // Options for the new Dataset image carousel
+    var datasetCarouselOptions = {
+      slidesToScroll: 1,
+      slidesToShow: 3, // Show 3 images to allow for scrolling
+      loop: true,
+      infinite: true,
+      autoplay: false,
+    };
+    // Initialize the dataset carousel with its specific options
+    bulmaCarousel.attach('#dataset-image-carousel', datasetCarouselOptions);
+
+    // --- END: MODIFIED CAROUSEL INITIALIZATION ---
+
+
+    /* The old generic initialization is replaced by the specific ones above.
     var options = {
 			slidesToScroll: 1,
 			slidesToShow: 3,
@@ -37,34 +66,11 @@ $(document).ready(function() {
 			autoplay: false,
 			autoplaySpeed: 3000,
     }
-
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
+    */
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
 
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
     preloadInterpolationImages();
 
     $('#interpolation-slider').on('input', function(event) {
@@ -74,5 +80,4 @@ $(document).ready(function() {
     $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
     bulmaSlider.attach();
-
-})
+});
